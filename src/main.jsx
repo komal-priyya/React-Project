a81e1078bd0aa7e22eacb9dddcdf0c0ecc13021c
Komal Priya <kryptonbenzene@gmail.com>
2026-01-12 15:15:19 +0530

    Updated login page

--- src/main.jsx
+++ src/main.jsx
@@ -15,24 +15,43 @@
    
   
 // )
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+
+// import ProductProvider from "./contexts/ProductContext";
+// import SidebarProvider from "./contexts/SidebarContext";
+// import CartProvider from "./contexts/CartContext";
+// import { BrowserRouter } from "react-router-dom";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+
+//   // <SidebarProvider>
+//     {/* <CartProvider> */}
+//       // {/* <ProductProvider> */}
+//         // <React.StrictMode>
+//         <BrowserRouter>
+//           <App />
+        
+// </BrowserRouter>
+//         // </React.StrictMode>
+//       // </ProductProvider>
+//     // </CartProvider>
+//   // </SidebarProvider>
+  
+
+// );
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-
-import ProductProvider from "./contexts/ProductContext";
-import SidebarProvider from "./contexts/SidebarContext";
-import CartProvider from "./contexts/CartContext";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <SidebarProvider>
-    <CartProvider>
-      <ProductProvider>
-        <React.StrictMode>
-          <App />
-        </React.StrictMode>
-      </ProductProvider>
-    </CartProvider>
-  </SidebarProvider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
